b469b3c8fad8dd7682d711a79e8811bd203e578a
Renato Alvarenga <otaner.alvarenga@gmail.com>
2015-04-25 22:37:11 -0500

    Add  dir FargoPalaceHotel
    Until now, just the index.php is working.

---
+++ FargoPalaceHotel/index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Fargo Palace Hotel</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+	<!--link rel="stylesheet/less" href="less/bootstrap.less" type="text/css" /-->
+	<!--link rel="stylesheet/less" href="less/responsive.less" type="text/css" /-->
+	<!--script src="js/less-1.3.3.min.js"></script-->
+	<!--append ‘#!watch’ to the browser URL, then refresh the page. -->
+	
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
+
+  <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+  <!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+  <![endif]-->
+
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/scripts.js"></script>
+</head>
+<body>
+<?php # HOME PAGE.
+
+# display header section.
+include ( 'header.html' ) ;
+
+?>
+<div class="carousel slide" id="carousel-275680">
+    <ol class="carousel-indicators">
+	<li class="active" data-slide-to="0" data-target="#carousel-275680">
+	</li>
+	<li data-slide-to="1" data-target="#carousel-275680">
+	</li>
+	<li data-slide-to="2" data-target="#carousel-275680">
+	</li>
+    </ol>
+    <div class="carousel-inner">
+	<div class="item active">
+            <img alt="" src="img/fargo-palace-hotel.jpg" >
+	    <div class="carousel-caption">
+		<h4>
+		    Fargo Palace Hotel
+		</h4>
+		<p>
+                    Situated in downtown Fargo overlooking the city's business, government and cultural districts, the 18-story Radisson Fargo is the second-tallest building in North Dakota and dominates the flatlands of the Red River Valley.
+		</p>
+	</div>
+    </div>
+    <div class="item">
+	<img alt="" src="img/ndsu.jpg">
+	<div class="carousel-caption">
+	    <h4>
+		Good attractions
+	    </h4>
+	    <p>
+                You can enjoy easy access to popular attractions like the Fargodome, North Dakota State University, the Fargo Civic Center and numerous shops, coffeehouses, galleries, museums and theaters in downtown Fargo.
+	    </p>
+	</div>
+    </div>
+    <div class="item">
+	<img alt="" src="img/suite-master.jpg">
+	<div class="carousel-caption">
+	    <h4>
+		Suite Master
+	    </h4>
+	    <p>
+		Experiment this deluxe room.
+	    </p>
+	</div>
+    </div>
+    </div> <a class="left carousel-control" href="#carousel-275680" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a> <a class="right carousel-control" href="#carousel-275680" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+</div>
+    <div class="jumbotron">
+	<h1>
+	    Welcome to the Fargo Palace Hotel!
+	</h1>
+	<p>
+            Our hotel's Euro-style hotel rooms and on-site amenities, including a well-equipped fitness center, free Wi-Fi, complimentary airport shuttle service, a business center and three excellent dining outlets, ensure you a memorable stay.
+	</p>
+	<p>
+	    <a class="btn btn-primary btn-large" href="#">More about us</a>
+	</p>
+    </div>
+</div>
+</div>
+</div>
