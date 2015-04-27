f7e75d27d627a084f41f3a78ca70375aa232c38f
Renato Alvarenga <otaner.alvarenga@gmail.com>
2015-04-26 20:52:34 -0500

    Add login function to the web site

---
+++ FargoPalaceHotel/home.php
@@ -0,0 +1,48 @@
+<?php
+include('header.html');
+?>
+
+<div class="container">
+	<div class="row clearfix">
+		<div class="col-md-12 column">
+			<div class="tabbable" id="tabs-209386">
+				<ul class="nav nav-tabs">
+					<li>
+						<a href="#panel-01" data-toggle="tab">Home</a>
+					</li>
+					<li class="active">
+						<a href="#panel-02" data-toggle="tab">Reserves</a>
+					</li>
+					<li class="active">
+						<a href="#panel-03" data-toggle="tab">Check out</a>
+					</li>
+					<li class="active">
+						<a href="#panel-04" data-toggle="tab">Log out</a>
+					</li>
+				</ul>
+				<div class="tab-content">
+					<div class="tab-pane" id="panel-01">
+						<p>
+							I'm in Home section.
+						</p>
+					</div>
+					<div class="tab-pane active" id="panel-02">
+						<p>
+							Howdy, I'm in Reserves section now.
+						</p>
+					</div>
+					<div class="tab-pane active" id="panel-03">
+						<p>
+							Howdy, I'm in Check ou section now.
+						</p>
+					</div>
+					<div class="tab-pane active" id="panel-04">
+						<p>
+							Howdy, I'm in Log out section now.
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
