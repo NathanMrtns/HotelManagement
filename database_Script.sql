0b40ec6ef35edd24f7d633ca1f4facfff3767a07
Nathan Barreto <nathanmrtns@gmail.com>
2015-04-27 17:56:42 -0500

    Complete script of database

--- database_Script.sql
+++ database_Script.sql
@@ -1,44 +1,117 @@
-#create schema hotel;
+create schema hotel;
 use hotel;
 
-create table IF NOT EXISTS user_table(
-userID int not null AUTO_INCREMENT,
-first_name varchar(50),
-last_name varchar(50),
-phone varchar (20),
-email varchar(50),
-adress varchar(200),
-zip_code varchar(15),
-password varchar(100),
-primary key(userID));
+CREATE TABLE IF NOT EXISTS user_table (
+    userID INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    phone VARCHAR(20),
+    email VARCHAR(50),
+    adress VARCHAR(200),
+    zip_code VARCHAR(15),
+    user_password VARCHAR(100),
+    PRIMARY KEY (userID)
+);
 
-create table IF NOT EXISTS room(
-room_number int,
-is_available boolean,
-capacity int,
-price double,
-description varchar(300),
-room_type varchar(100),
-primary key(room_number));
+CREATE TABLE IF NOT EXISTS room (
+    room_number INT,
+    capacity INT,
+    price DECIMAL(8,2),
+    description VARCHAR(300),
+    room_type VARCHAR(100),
+    PRIMARY KEY (room_number)
+);
 
-create table IF NOT EXISTS reservation(
-reservationID int auto_increment,
-room_number int,
-userID int,
-start_date date,
-end_date date,
-PRIMARY KEY (reservationID),
-FOREIGN KEY (room_number) REFERENCES room(room_number),
-FOREIGN KEY (userID) REFERENCES user_table(userID));
+CREATE TABLE IF NOT EXISTS reservation (
+    reservationID INT AUTO_INCREMENT,
+    room_number INT,
+    userID INT,
+    start_date DATE,
+    end_date DATE,
+    PRIMARY KEY (reservationID),
+    FOREIGN KEY (room_number)
+        REFERENCES room (room_number),
+    FOREIGN KEY (userID)
+        REFERENCES user_table (userID)
+);
 
-create table IF NOT EXISTS invoice(
-invoiceID int not null auto_increment,
-userID int,
-reservationID int,
-total_price double,
-PRIMARY KEY (invoiceID),
-FOREIGN KEY (userID) REFERENCES user_table(userID),
-FOREIGN KEY (reservationID) REFERENCES reservation(reservationID));
+CREATE TABLE IF NOT EXISTS invoice (
+    invoiceID INT NOT NULL AUTO_INCREMENT,
+    userID INT,
+    reservationID INT,
+    total_price DOUBLE,
+    PRIMARY KEY (invoiceID),
+    FOREIGN KEY (userID)
+        REFERENCES user_table (userID),
+    FOREIGN KEY (reservationID)
+        REFERENCES reservation (reservationID)
+);
+
+insert into room
+values (101,1,69.00,"Standard room with 1 single bed, tv, and bathroom.","Standard");
+insert into room
+values (102,2,89.00,"Standard room with 1 king size bed, tv, and bathroom.","Standard");
+insert into room
+values (103,2,89.00,"Standard room with 1 king size bed, tv, and bathroom.","Standard");
+insert into room
+values (104,3,109.00,"Standard room with 1 king size bed and 1 single bed, tv, and bathroom.","Standard");
+insert into room
+values (105,4,119.00,"Standard room with 2 king size beds, tv and bathroom.","Standard");
+
+insert into room
+values (301,1,69.00,"Standard room with 1 single bed, tv, and bathroom.","Standard");
+insert into room
+values (302,2,89.00,"Standard room with 1 king size bed, tv, and bathroom.","Standard");
+insert into room
+values (303,2,89.00,"Standard room with 1 king size bed, tv, and bathroom.","Standard");
+insert into room
+values (304,3,109.00,"Standard room with 1 king size bed and 1 single bed, tv, and bathroom.","Standard");
+insert into room
+values (305,4,119.00,"Standard room with 2 king size beds, tv and bathroom.","Standard");
+
+insert into room
+values (601,1,69.00,"Standard room with 1 single bed, tv, and bathroom.","Standard");
+insert into room
+values (602,2,89.00,"Standard room with 1 king size bed, tv, and bathroom.","Standard");
+insert into room
+values (603,2,89.00,"Standard room with 1 king size bed, tv, and bathroom.","Standard");
+insert into room
+values (604,3,109.00,"Standard room with 1 king size bed and 1 single bed, tv, and bathroom.","Standard");
+insert into room
+values (605,4,119.00,"Standard room with 2 king size beds, tv and bathroom.","Standard");
+
+insert into room
+values (201,1,89.00,"Prime room with 1 single bed, tv, and bathtub.","Prime");
+insert into room
+values (202,2,109.00,"Prime room with 1 king size bed, tv, and bathtub.","Prime");
+insert into room
+values (203,2,109.00,"Prime room with 1 king size bed, tv, and bathtub.","Prime");
+insert into room
+values (204,3,129.00,"Prime room with 1 king size bed and 1 single bed, tv, and bathtub.","Prime");
+insert into room
+values (205,4,149.00,"Prime room with 2 king size beds, tv and bathtub.","Prime");
+
+insert into room
+values (401,1,89.00,"Prime room with 1 single bed, tv, and bathtub.","Prime");
+insert into room
+values (402,2,109.00,"Prime room with 1 king size bed, tv, and bathtub.","Prime");
+insert into room
+values (403,2,109.00,"Prime room with 1 king size bed, tv, and bathtub.","Prime");
+insert into room
+values (404,3,129.00,"Prime room with 1 king size bed and 1 single bed, tv, and bathtub.","Prime");
+insert into room
+values (405,4,149.00,"Prime room with 2 king size beds, tv and bathtub.","Prime");
+
+insert into room
+values (701,1,89.00,"Prime room with 1 single bed, tv, and bathtub.","Prime");
+insert into room
+values (702,2,109.00,"Prime room with 1 king size bed, tv, and bathtub.","Prime");
+insert into room
+values (703,2,109.00,"Prime room with 1 king size bed, tv, and bathtub.","Prime");
+insert into room
+values (704,3,129.00,"Prime room with 1 king size bed and 1 single bed, tv, and bathtub.","Prime");
+insert into room
+values (705,4,149.00,"Prime room with 2 king size beds, tv and bathtub.","Prime");
 
 
 
