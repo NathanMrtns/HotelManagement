a32539794687ffce292d5060cf0344931f78ecc4
Nathan Barreto <nathanmrtns@gmail.com>
2015-04-28 00:46:41 -0500

    Small fix in header and search partial working

--- FargoPalaceHotel/reserve.php
+++ FargoPalaceHotel/reserve.php
@@ -1,28 +1,29 @@
 
 <?php
+session_start();
 include('header.html');
+
 ?>
-<html lang="en">
-<head>
-  <script>
+
+<script>
   $(function() {
     $( "#startDate,#endDate" ).datepicker();
   });
-  </script>
-</head>
+</script>
 
 <body>
-	<form class="form-inline" method="post" >
+	<form class="form-inline" method="post">
 		<div class="input-group input-group-sm">
 		    <div class="col-xs-3">
-	    		<span>Start Date: <input class="form-control" type="text" name="startDate" id="startDate"><span>
+	    		<span>Start Date: <input class="form-control" type="text" name="startDate" id="startDate" required><span>
 	    	</div>
 	    	<div class="col-xs-3">
-				<span>End Date: <input class="form-control" type="text" name="endDate" id="endDate"></span>
+				<span>End Date: <input class="form-control" type="text" name="endDate" id="endDate" required></span>
 			</div>
 			<div class="col-xs-2">
 				<p>Room Capacity: 
-					<select class="form-control">
+					<select class="form-control" name="capacity">
+						<option value"">Capacity</option>
 						<option value="1">1</option>
 						<option value="2">2</option>
 						<option value="3">3</option>
@@ -32,10 +33,73 @@ include('header.html');
 				</p>
 			</div>
 			<div class="col-xs-2">
+				<p>Room Type: 
+					<select class="form-control" name="rtype">
+						<option value"">Room type</option>
+						<option value="Standard">Standard</option>
+						<option value="Prime">Prime</option>
+					</select>
+				</p>
+			</div>
+			<div class="col-xs-2">
 				<br>
-				<button type="submit" class="btn btn-default">Search Rooms</button>
+				<button type="submit" name="searchBtn" value"search" class="btn btn-default">Search Rooms</button>
 			</div>
 		</div>
 	</form>
+<?php
+	if (isset($_POST['startDate']) && isset($_POST['endDate']) && isset($_POST['capacity'])) {
+    $_SESSION['startDate'] = $_POST['startDate'];
+    $_SESSION['endDate'] = $_POST['endDate'];
+    $_SESSION['capacity'] = $_POST['capacity'];
+    $_SESSION['rtype'] = $_POST['rtype'];
+
+	//connect to MySQL (host, user_name, password)
+	require('connect_db.php');
+
+	$query = 'select room_number, capacity, description, room_type, price 
+		      from room 
+	          where capacity = '. "'" . $_SESSION['capacity'] . "'"
+	          . "and room_type = " . "'" . $_SESSION['rtype'] . "'";
+
+	if(!($result = mysqli_query($dbc, $query)))
+	{
+	    print ("Coudnot execute query! <br/>");
+	    die(mysql_error());
+	}
+	print '<div class="panel panel-default">';
+    	print '<div class="panel-heading">Search Results</div>';
+			print "<table class='table'>";
+				print "<tr><td>Room Number</td><td>Capacity</td><td>Description</td><td>Type</td><td>Price</td>";
+				while($row = mysqli_fetch_array($result))
+			          {
+			          echo "<tr><td>" . $row['room_number'] . "</td><td> " . $row['capacity'] . "</td><td> " . $row['description'] . "</td><td> " . $row['room_type'] . "</td><td> $" . $row['price'] . "</td></tr>"; //these are the fields that you have stored in your database table employee
+			          }
+				/*for($c=0; $row = mysqli_fetch_row($result); $c++)
+				{
+				    print("<tr>");
+				    foreach($row as $key => $value)
+				    	if(substr($value, -3) == ".00"){
+							print ("<td style=width:200px>$ $value</td>");
+						}else{
+							print ("<td style=width:200px>$value</td>");
+						}	        
+				    print("</tr>");
+				}*/
+			print"</table>";
+		print "</div>";
+	print "</div>";
+	mysqli_close($dbc);
+    exit;
+}else{
+    if(isset($_SESSION['startDate']) && isset($_SESSION['endDate']) && isset($_SESSION['capacity'])) {
+        unset($_SESSION['startDate']);
+        unset($_SESSION['endDate']);
+        unset($_SESSION['capacity']);
+        print "" ;
+	}
+}
+?>
 </body>
+
 </html>
