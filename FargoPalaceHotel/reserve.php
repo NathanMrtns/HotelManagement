0e79ca20980d4b674a7c4ffd005f1d6bf00589eb
Nathan Barreto <nathanmrtns@gmail.com>
2015-04-28 21:28:14 -0500

    Range of datepickers fixed

--- FargoPalaceHotel/reserve.php
+++ FargoPalaceHotel/reserve.php
@@ -11,9 +11,30 @@ else{
 ?>
 
 <script>
-  $(function() {
-    $( "#startDate,#endDate" ).datepicker();
-  });
+	$(document).ready(function () {
+   		//$("#endDate").datepicker({
+   		//	disabled: true
+   		//})
+   		var $startDate = $("#startDate");
+   		$startDate.datepicker({
+   			minDate: 0,
+   			onSelect: function(date){
+   				var selectedDate = $("#startDate").datepicker('getDate');
+   				$("#endDate").datepicker('option', "minDate", selectedDate);
+   			}
+   		})
+   		if($("#startDate").datepicker('getDate')==null){
+   			var minDate = 0;
+   		}
+   		$("#endDate").datepicker({
+   			minDate: minDate,
+   			onSelect: function(date){
+   				var selectedEndDate = $("#endDate").datepicker('getDate');
+   				$("#startDate").datepicker('option', "maxDate", selectedEndDate);
+   			}
+   		})
+
+  	});
 </script>
 
 <body>
